7ec1d8ac58459ddc120db87edf6e911251f7822d
Viktor Van Puyvelde <viktor.vanpuyvelde2@student.hogent.be>
2022-04-26 11:56:35 +0200

    first commit

---
+++ src/main/java/com/springBoot/EWDJ_Opdracht/EwdjOpdrachtApplication.java
@@ -0,0 +1,13 @@
+package com.springBoot.EWDJ_Opdracht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwdjOpdrachtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EwdjOpdrachtApplication.class, args);
+	}
+
+}
